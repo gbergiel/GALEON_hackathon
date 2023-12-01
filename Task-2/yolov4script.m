69e1c4e079b18e508cc58935301fe08bfd7e75f5
MatSol9 <mateusz.soltys@protonmail.com>
2023-12-01 23:32:27 +0100

    Added NN training

--- Task-2/yolov4script.m
+++ Task-2/yolov4script.m
@@ -43,5 +43,61 @@ I = data{1};
 bbox = data{2};
 annotatedImage = insertShape(I,"Rectangle",bbox);
 annotatedImage = imresize(annotatedImage,2);
+% figure
+% imshow(annotatedImage)
+
+inputSize = size(annotatedImage);
+className = "BoundingBox";
+rng("default")
+trainingDataForEstimation = transform(trainingData,@(data)preprocessData(data,inputSize));
+numAnchors = 9;
+[anchors,meanIoU] = estimateAnchorBoxes(trainingDataForEstimation,numAnchors);
+area = anchors(:, 1).*anchors(:,2);
+[~,idx] = sort(area,"descend");
+
+anchors = anchors(idx,:);
+anchorBoxes = {anchors(1:3,:)
+    anchors(4:6,:)
+    anchors(7:9,:)
+    };
+detector = yolov4ObjectDetector("csp-darknet53-coco",className,anchorBoxes,InputSize=inputSize);
+
+options = trainingOptions("adam",...
+    GradientDecayFactor=0.9,...
+    SquaredGradientDecayFactor=0.999,...
+    InitialLearnRate=0.001,...
+    LearnRateSchedule="none",...
+    MiniBatchSize=4,...
+    L2Regularization=0.0005,...
+    MaxEpochs=70,...
+    BatchNormalizationStatistics="moving",...
+    DispatchInBackground=true,...
+    ResetInputNormalization=false,...
+    Shuffle="every-epoch",...
+    VerboseFrequency=20,...
+    ValidationFrequency=1000,...
+    CheckpointPath=tempdir,...
+    ValidationData=validationData);
+doTraining = true;
+if doTraining       
+    % Train the YOLO v4 detector.
+    [detector,info] = trainYOLOv4ObjectDetector(trainingData,detector,options);
+else
+    % Load pretrained detector for the example.
+    detector = downloadPretrainedYOLOv4Detector();
+end
+I = imread("Task-2/Training Dataset/WBA7F21070B235942_20230406_110938_0104_27723.png");
+[bboxes,scores,labels] = detect(detector,I);
+I = insertObjectAnnotation(I,"rectangle",bboxes,scores);
 figure
-imshow(annotatedImage)
+imshow(I)
+
+function data = preprocessData(data,targetSize)
+% Resize image and bounding boxes to the targetSize.
+    scale = targetSize(1:2)./size(data{1},[1 2]);
+    data{1} = imresize(data{1},targetSize(1:2));
+    boxEstimate=round(data{2});
+    boxEstimate(:,1)=max(boxEstimate(:,1),1);
+    boxEstimate(:,2)=max(boxEstimate(:,2),1);
+    data{2} = bboxresize(boxEstimate,scale);
+end
